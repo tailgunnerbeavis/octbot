d7cc69f827de792eb4f6fdaf05587549a73a0823
Dan Johnson <dan.h.johnson@gmail.com>
2013-05-02 22:46:38 -0700

    add quickgrab.py

---
+++ quickgrab.py
@@ -0,0 +1,15 @@
+from PIL import ImageGrab
+import os
+import time
+import win32gui
+ 
+def screenGrab():
+    box = ()
+    im = ImageGrab.grab()
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+ 
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
