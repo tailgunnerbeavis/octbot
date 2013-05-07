3a50ce63b2dc7949fe4febe57b4ca5bb9668fda8
tailgunnerbeavis <dan.h.johnson@gmail.com>
2013-05-06 18:54:42 -0700

    added functions
    Will now try to screenshot only the octgn window, though gets confused
    if there are multiple octgn windows.

--- quickgrab.py
+++ quickgrab.py
@@ -3,13 +3,35 @@ import os
 import time
 import win32gui
  
-def screenGrab():
-    box = ()
-    im = ImageGrab.grab()
+def screenGrab(box):
+    im = ImageGrab.grab(box)
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
- 
+
+def winEnumHandler( hwnd, ctx ):
+    whereIsOctgn = ()
+    coords = ()
+    if win32gui.IsWindowVisible( hwnd ):
+        if win32gui.GetWindowText( hwnd ) == 'Octgn  version : 3.1.16.99 : Android-Netrunner':
+            whereIsOctgn = hwnd
+            print (hex(whereIsOctgn))
+            coords = win32gui.GetWindowRect(whereIsOctgn)
+            print (coords)
+            win32gui.SetForegroundWindow(whereIsOctgn)
+            screenGrab(coords)
+
+def winListHandler( hwnd, ctx ):
+    if win32gui.IsWindowVisible( hwnd ):
+        print (hex( hwnd ), win32gui.GetWindowText( hwnd ))
+
+def listWindows():
+    win32gui.EnumWindows( winListHandler, None )
+
+def screenGrabOctgn():
+    win32gui.EnumWindows( winEnumHandler, None )
+         
 def main():
-    screenGrab()
+    screenGrabOctgn()
+    listWindows()
  
 if __name__ == '__main__':
     main()
